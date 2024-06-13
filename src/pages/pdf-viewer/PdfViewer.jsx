6428d4dbc5afa2452d249f2858e0e821d97537f4
Kavindya Ranaweera <ranaweerakdilshani@gmail.com>
2024-06-13 20:21:23 +0530

    add functions to download selected pdf

--- src/pages/pdf-viewer/PdfViewer.jsx
+++ src/pages/pdf-viewer/PdfViewer.jsx
@@ -6,7 +6,7 @@ import { apiEndpoints } from "../../endpoints/endpoints";
 const PdfViewer = () => {
   // Get the 'id' parameter from the URL
   const { documentId } = useParams();
-  const [file, setFile] = useState(null);
+  const [fileTitle, setFileTitle] = useState("");
 
   useEffect(() => {
     const getFiles = async () => {
@@ -19,8 +19,42 @@ const PdfViewer = () => {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
           },
+          responseType: "blob",
         });
-        setFile(response?.data?.file);
+
+        // Handle response
+        if (response.status === 200) {
+          // Extract the filename from the Content-Disposition header
+          const contentDisposition = response.headers["content-disposition"];
+          const filename = contentDisposition
+            ? contentDisposition.split("filename=")[1].replace(/"/g, "")
+            : "unknown.pdf";
+          setFileTitle(filename);
+
+          // Create a blob object from the response data
+          const blob = new Blob([response.data], {
+            type: response.headers["content-type"],
+          });
+
+          // Create a temporary URL for the blob
+          const url = URL.createObjectURL(blob);
+
+          // Create a link element
+          const a = document.createElement("a");
+          a.style.display = "none";
+          a.href = url;
+          a.download = filename;
+
+          // Append the link to the body and click it
+          document.body.appendChild(a);
+          a.click();
+
+          // Clean up
+          URL.revokeObjectURL(url);
+          document.body.removeChild(a);
+        } else {
+          throw new Error("Failed to fetch PDF");
+        }
       } catch (error) {
         console.error("Error fetching files:", error);
       }
@@ -31,7 +65,16 @@ const PdfViewer = () => {
     }
   }, [documentId]);
 
-  return <div>{documentId}</div>;
+  return (
+    <>
+      <div className="container">
+        <p className="py-5">
+          Your selected file, <span className="fw-bold">{fileTitle}</span> ,
+          will be downloaded...
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default PdfViewer;
